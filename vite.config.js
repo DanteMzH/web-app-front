270c3709e6d46ffb777b2b0c9471c784078a8cd6
timoteoH <timoteodantemunizhuaman@gmail.com>
2024-01-08 10:29:16 -0500

    Actualizar dependencias de producciónes7

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
+// Obtén el puerto del entorno o utiliza 3000 por defecto para desarrollo
+const port = process.env.PORT || 3000;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? `https://frontendnotes-dcda18d7dadc.herokuapp.com:${port}/` : '/',
   build: {
     outDir: 'dist', // O el directorio que desees
     rollupOptions: {
